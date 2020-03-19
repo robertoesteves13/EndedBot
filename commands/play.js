20bec94db8c8456e5424c05014acb8b80879c054
Roberto Esteves <robertoesteves@protonmail.com>
2020-03-19 01:13:32 -0300

    Change ytdl-core to a more optimized for bots

--- commands/play.js
+++ commands/play.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'play',
     description: 'Plays audio or music via youtube',
     async execute(message){
-        const ytdl = require('ytdl-core');
+        const ytdl = require('ytdl-core-discord');
 
         const args = message.content.split(' ');
         const connection = await message.member.voice.channel.join();
-        connection.play(ytdl(args[1], { filter: 'audioonly' }));
+        connection.play(await ytdl(args[1]), { type: 'opus' });
     }
 }
